267c7aa0f340b0793eb2f2a2860d09a8e414e240
Shahzad Aslam <sa@digishares.io>
2021-07-03 21:14:44 +0500

    some data added

---
+++ erc1404.js
@@ -0,0 +1,585 @@
+'use strict'  
+
+const Web3 = require('web3');
+const ethereumjs = require('ethereumjs-tx');
+var Accounts = require('web3-eth-accounts');
+const async = require('async');
+const ethereum = require("./modules/ethereum");
+const fs = require('fs');
+
+var abi_contract = [
+    {
+      "constant": false,
+      "inputs": [
+        {
+          "name": "_spender",
+          "type": "address"
+        },
+        {
+          "name": "_value",
+          "type": "uint256"
+        }
+      ],
+      "name": "approve",
+      "outputs": [
+        {
+          "name": "",
+          "type": "bool"
+        }
+      ],
+      "payable": false,
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "constant": true,
+      "inputs": [
+        {
+          "name": "_operator",
+          "type": "address"
+        },
+        {
+          "name": "_role",
+          "type": "string"
+        }
+      ],
+      "name": "checkRole",
+      "outputs": [],
+      "payable": false,
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "constant": true,
+      "inputs": [],
+      "name": "SUCCESS_CODE",
+      "outputs": [
+        {
+          "name": "",
+          "type": "uint8"
+        }
+      ],
+      "payable": false,
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "constant": true,
+      "inputs": [],
+      "name": "totalSupply",
+      "outputs": [
+        {
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "payable": false,
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "constant": true,
+      "inputs": [],
+      "name": "ROLE_WHITELISTED",
+      "outputs": [
+        {
+          "name": "",
+          "type": "string"
+        }
+      ],
+      "payable": false,
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "constant": true,
+      "inputs": [
+        {
+          "name": "_operator",
+          "type": "address"
+        },
+        {
+          "name": "_role",
+          "type": "string"
+        }
+      ],
+      "name": "hasRole",
+      "outputs": [
+        {
+          "name": "",
+          "type": "bool"
+        }
+      ],
+      "payable": false,
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "constant": true,
+      "inputs": [],
+      "name": "NON_WHITELIST_ERROR",
+      "outputs": [
+        {
+          "name": "",
+          "type": "string"
+        }
+      ],
+      "payable": false,
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "constant": false,
+      "inputs": [
+        {
+          "name": "from",
+          "type": "address"
+        },
+        {
+          "name": "to",
+          "type": "address"
+        },
+        {
+          "name": "value",
+          "type": "uint256"
+        }
+      ],
+      "name": "transferFrom",
+      "outputs": [
+        {
+          "name": "success",
+          "type": "bool"
+        }
+      ],
+      "payable": false,
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "constant": true,
+      "inputs": [],
+      "name": "NON_WHITELIST_CODE",
+      "outputs": [
+        {
+          "name": "",
+          "type": "uint8"
+        }
+      ],
+      "payable": false,
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "constant": false,
+      "inputs": [
+        {
+          "name": "_operators",
+          "type": "address[]"
+        }
+      ],
+      "name": "removeAddressesFromWhitelist",
+      "outputs": [],
+      "payable": false,
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "constant": false,
+      "inputs": [
+        {
+          "name": "_operator",
+          "type": "address"
+        }
+      ],
+      "name": "removeAddressFromWhitelist",
+      "outputs": [],
+      "payable": false,
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "constant": false,
+      "inputs": [
+        {
+          "name": "_spender",
+          "type": "address"
+        },
+        {
+          "name": "_subtractedValue",
+          "type": "uint256"
+        }
+      ],
+      "name": "decreaseApproval",
+      "outputs": [
+        {
+          "name": "",
+          "type": "bool"
+        }
+      ],
+      "payable": false,
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "constant": true,
+      "inputs": [
+        {
+          "name": "_owner",
+          "type": "address"
+        }
+      ],
+      "name": "balanceOf",
+      "outputs": [
+        {
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "payable": false,
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "constant": false,
+      "inputs": [],
+      "name": "renounceOwnership",
+      "outputs": [],
+      "payable": false,
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "constant": false,
+      "inputs": [
+        {
+          "name": "_operator",
+          "type": "address"
+        }
+      ],
+      "name": "addAddressToWhitelist",
+      "outputs": [],
+      "payable": false,
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "constant": true,
+      "inputs": [
+        {
+          "name": "restrictionCode",
+          "type": "uint8"
+        }
+      ],
+      "name": "messageForTransferRestriction",
+      "outputs": [
+        {
+          "name": "message",
+          "type": "string"
+        }
+      ],
+      "payable": false,
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "constant": true,
+      "inputs": [],
+      "name": "owner",
+      "outputs": [
+        {
+          "name": "",
+          "type": "address"
+        }
+      ],
+      "payable": false,
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "constant": true,
+      "inputs": [
+        {
+          "name": "_operator",
+          "type": "address"
+        }
+      ],
+      "name": "whitelist",
+      "outputs": [
+        {
+          "name": "",
+          "type": "bool"
+        }
+      ],
+      "payable": false,
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "constant": false,
+      "inputs": [
+        {
+          "name": "to",
+          "type": "address"
+        },
+        {
+          "name": "value",
+          "type": "uint256"
+        }
+      ],
+      "name": "transfer",
+      "outputs": [
+        {
+          "name": "success",
+          "type": "bool"
+        }
+      ],
+      "payable": false,
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "constant": false,
+      "inputs": [
+        {
+          "name": "_spender",
+          "type": "address"
+        },
+        {
+          "name": "_addedValue",
+          "type": "uint256"
+        }
+      ],
+      "name": "increaseApproval",
+      "outputs": [
+        {
+          "name": "",
+          "type": "bool"
+        }
+      ],
+      "payable": false,
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "constant": true,
+      "inputs": [
+        {
+          "name": "_owner",
+          "type": "address"
+        },
+        {
+          "name": "_spender",
+          "type": "address"
+        }
+      ],
+      "name": "allowance",
+      "outputs": [
+        {
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "payable": false,
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "constant": false,
+      "inputs": [
+        {
+          "name": "_operators",
+          "type": "address[]"
+        }
+      ],
+      "name": "addAddressesToWhitelist",
+      "outputs": [],
+      "payable": false,
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "constant": true,
+      "inputs": [],
+      "name": "SUCCESS_MESSAGE",
+      "outputs": [
+        {
+          "name": "",
+          "type": "string"
+        }
+      ],
+      "payable": false,
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "constant": false,
+      "inputs": [
+        {
+          "name": "_newOwner",
+          "type": "address"
+        }
+      ],
+      "name": "transferOwnership",
+      "outputs": [],
+      "payable": false,
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "inputs": [],
+      "payable": false,
+      "stateMutability": "nonpayable",
+      "type": "constructor"
+    },
+    {
+      "anonymous": false,
+      "inputs": [
+        {
+          "indexed": true,
+          "name": "operator",
+          "type": "address"
+        },
+        {
+          "indexed": false,
+          "name": "role",
+          "type": "string"
+        }
+      ],
+      "name": "RoleAdded",
+      "type": "event"
+    },
+    {
+      "anonymous": false,
+      "inputs": [
+        {
+          "indexed": true,
+          "name": "operator",
+          "type": "address"
+        },
+        {
+          "indexed": false,
+          "name": "role",
+          "type": "string"
+        }
+      ],
+      "name": "RoleRemoved",
+      "type": "event"
+    },
+    {
+      "anonymous": false,
+      "inputs": [
+        {
+          "indexed": true,
+          "name": "previousOwner",
+          "type": "address"
+        }
+      ],
+      "name": "OwnershipRenounced",
+      "type": "event"
+    },
+    {
+      "anonymous": false,
+      "inputs": [
+        {
+          "indexed": true,
+          "name": "previousOwner",
+          "type": "address"
+        },
+        {
+          "indexed": true,
+          "name": "newOwner",
+          "type": "address"
+        }
+      ],
+      "name": "OwnershipTransferred",
+      "type": "event"
+    },
+    {
+      "anonymous": false,
+      "inputs": [
+        {
+          "indexed": true,
+          "name": "owner",
+          "type": "address"
+        },
+        {
+          "indexed": true,
+          "name": "spender",
+          "type": "address"
+        },
+        {
+          "indexed": false,
+          "name": "value",
+          "type": "uint256"
+        }
+      ],
+      "name": "Approval",
+      "type": "event"
+    },
+    {
+      "anonymous": false,
+      "inputs": [
+        {
+          "indexed": true,
+          "name": "from",
+          "type": "address"
+        },
+        {
+          "indexed": true,
+          "name": "to",
+          "type": "address"
+        },
+        {
+          "indexed": false,
+          "name": "value",
+          "type": "uint256"
+        }
+      ],
+      "name": "Transfer",
+      "type": "event"
+    },
+    {
+      "constant": true,
+      "inputs": [
+        {
+          "name": "from",
+          "type": "address"
+        },
+        {
+          "name": "to",
+          "type": "address"
+        },
+        {
+          "name": "value",
+          "type": "uint256"
+        }
+      ],
+      "name": "detectTransferRestriction",
+      "outputs": [
+        {
+          "name": "restrictionCode",
+          "type": "uint8"
+        }
+      ],
+      "payable": false,
+      "stateMutability": "view",
+      "type": "function"
+    }
+  ];
+var abi_service =  [];
+
+var contract_address = "0x68bd9e78f74b8a8eadC6E9bFAcBAc715937B7Cbb";
+var service_address= "0x65d5b5811a64872b799d72edfb225eabfc924c09";
+var linkToBlockchainServer = "https://kovan.infura.io/v3/fe41724da6f24b76a782f376b2698ee8";
+//var linkToBlockchainServer = "http://127.0.0.1:7545";
+
+
+if (process.argv[2] == "etherBalance") {
+
+	// node PolyMath etherBalance 0xea1466402fc4b0a0b4959e4cd040e79a7309b3c9
+	ethereum.getAccountEtherBalance( process.argv[3], abi_contract, contract_address, linkToBlockchainServer).then(function(data){
+		console.log(data);
+        process.exit(0);
+	})
+
+}
--- ravencoin.js
+++ ravencoin.js
@@ -1,11 +1,403 @@
 const async = require('async');
 const fs = require('fs');
+const axios = require("axios");
+
+const CONFIG = getConfig();
+
+const serverLink = "http://127.0.0.1:8766";
+//  http://127.0.0.1:8766        main net
+//  http://127.0.0.1:18766      testnet
+  
 
 
 if (process.argv[2] == "test") {
+    //  node ravencoin test configfile=./data/ravencoinconfig.txt
+        
+        rpc("getblockcount", [])
+            .then(function (data) {
 
-    //node ravencoin test
-    
-    console.log("hhhhhhhhhhh")
+                console.log("Holders of", CONFIG.asset);
+                console.table(data);
+          })
+          .catch(function (e) {
+                console.error(e.message);
+          });
     
 }
+
+// RVN
+if (process.argv[2] == "getaddressbalance") {
+    //  node ravencoin getaddressbalance configfile=./data/ravencoinconfig.txt
+        
+        rpc("getaddressbalance", ["moCyJzkXNxBpB1PzkLEGc4FQfC11zjSyqT"])
+            .then(function (data) {
+
+                console.log("Holders of", CONFIG.asset);
+                console.log(data);
+          })
+          .catch(function (e) {
+                console.error(e.message);
+          });
+    
+}
+
+
+// Assets 
+if (process.argv[2] == "listaddressesfortag") {
+        // node ravencoin listaddressesfortag configfile=./data/ravencoinconfig.txt
+        
+        rpc("listaddressesfortag", ["#MEGA"])
+            .then(function (data) {
+
+                console.log(data);
+          })
+          .catch(function (e) {
+                console.error(e.message);
+          });
+    
+}	
+
+if (process.argv[2] == "listaddressesbyassets") {
+    //  node ravencoin listaddressesbyassets configfile=./data/ravencoinconfig.txt
+        
+        rpc("listaddressesbyasset", ["$MEGATECH"])
+            .then(function (data) {
+                console.log(data);
+          })
+          .catch(function (e) {
+                console.error(e.message);
+          });
+    
+}     //get all addresses that has tag + their balances
+
+if (process.argv[2] == "listmyassets") {
+    //  node ravencoin listmyassets configfile=./data/ravencoinconfig.txt
+        
+        rpc("listmyassets", ["DIGICOIN"])
+            .then(function (data) {
+
+                console.log(CONFIG.asset);
+                console.log(data);
+          })
+          .catch(function (e) {
+                console.error(e.message);
+          });
+    
+}
+
+if (process.argv[2] == "freezerestrictedasset") {
+    	//  node ravencoin freezerestrictedasset configfile=./data/ravencoinconfig.txt
+        rpc("freezerestrictedasset", ["$DIGICOIN"])
+            .then(function (data) {
+                console.log(data);
+          })
+          .catch(function (e) {
+				console.log("............")
+                console.error(e.message);
+          });    
+}     
+
+if (process.argv[2] == "freezeaddress") {
+    	//  node ravencoin freezeaddress configfile=./data/ravencoinconfig.txt
+        rpc("freezeaddress", ["$MEGATECH", "n4dQ28t5LyJf3pW5w6m8F9mvXaQ6ksWEwM"])
+            .then(function (data) {
+                console.log(data);
+          })
+          .catch(function (e) {
+				console.log("............")
+                console.error(e.message);
+          });    
+}     
+
+if (process.argv[2] == "unfreezeaddress") {
+    	//  node ravencoin unfreezeaddress configfile=./data/ravencoinconfig.txt
+        rpc("unfreezeaddress", ["$MEGATECH", "n4dQ28t5LyJf3pW5w6m8F9mvXaQ6ksWEwM"])
+            .then(function (data) {
+                console.log(data);
+          })
+          .catch(function (e) {
+				console.log("............")
+                console.error(e.message);
+          });    
+}
+
+if (process.argv[2] == "listaddressrestrictions") {
+    	//  node ravencoin listaddressrestrictions configfile=./data/ravencoinconfig.txt
+        rpc("listaddressrestrictions", ["mvGA9tP6aAD3dcSHLEWcLJADg1ryKf14Pu"])
+            .then(function (data) {
+                console.log(data);
+          })
+          .catch(function (e) {
+				console.log("............")
+                console.error(e.message);
+          });    
+}
+
+if (process.argv[2] == "viewmyrestrictedaddresses") {
+    	//  node ravencoin viewmyrestrictedaddresses configfile=./data/ravencoinconfig.txt
+        rpc("viewmyrestrictedaddresses", [])
+            .then(function (data) {
+                console.log(data);
+          })
+          .catch(function (e) {
+				console.log("............")
+                console.error(e.message);
+          });    
+}
+
+if (process.argv[2] == "addtagtoaddress") {
+    	//  node ravencoin addtagtoaddress configfile=./data/ravencoinconfig.txt
+        rpc("addtagtoaddress", ["#MEGA", "myhyhmRiKnuqgpmyAAeujWoxoTyWpZVUi7"])
+            .then(function (data) {
+                console.log(data);
+          })
+          .catch(function (e) {
+                console.error(e.message);
+          });
+}
+
+if (process.argv[2] == "listassets") {
+    	//  node ravencoin listassets configfile=./data/ravencoinconfig.txt
+        rpc("listassets", [])
+            .then(function (data) {
+                console.log(data);
+          })
+          .catch(function (e) {
+                console.error(e.message);
+          });
+}
+
+if (process.argv[2] == "listmyassets") {
+    	//  node ravencoin listmyassets configfile=./data/ravencoinconfig.txt
+        rpc("listmyassets", [])
+            .then(function (data) {
+                console.log(data);
+          })
+          .catch(function (e) {
+                console.error(e.message);
+          });
+}
+
+
+
+// Address
+if (process.argv[2] == "getaddresstxids") {
+    //  node ravencoin getaddresstxids configfile=./data/ravencoinconfig.txt
+        
+        rpc("getaddresstxids", ["mwC16XZ9Tq9n9FWk9iRfBetAGGtD8C11Rt"])
+            .then(function (data) {
+                console.log(data);
+          })
+          .catch(function (e) {
+                console.error(e.message);
+          });
+
+}
+
+if (process.argv[2] == "getassetdata") {
+    //  node ravencoin getassetdata configfile=./data/ravencoinconfig.txt
+        
+        rpc("getassetdata", ["DIGICOIN"])
+            .then(function (data) {
+                console.log(data);
+          })
+          .catch(function (e) {
+                console.error(e.message);
+          });
+    
+}
+
+if (process.argv[2] == "listassetbalancesbyaddress") {
+    	//  node ravencoin listassetbalancesbyaddress configfile=./data/ravencoinconfig.txt
+        rpc("listassetbalancesbyaddress", ["myxd8nhb8Bh9h9syK2n2u6hKRyrSrAbxZ8"])
+            .then(function (data) {
+                console.log(data);
+          })
+          .catch(function (e) {
+                console.error(e.message);
+          });
+}
+
+if (process.argv[2] == "checkaddresstag") {
+        // node ravencoin checkaddresstag configfile=./data/ravencoinconfig.txt
+
+        rpc("checkaddresstag", ["myhyhmRiKnuqgpmyAAeujWoxoTyWpZVUi7", "#MEGA"])
+            .then(function (data) {
+
+                console.log(data);
+          })
+          .catch(function (e) {
+                console.error(e.message);
+          });
+    
+}
+
+if (process.argv[2] == "listtagsforaddress") {
+        // node ravencoin listtagsforaddress configfile=./data/ravencoinconfig.txt
+
+        rpc("listtagsforaddress", ["munskw5HqVKW5KKuALq5etxRECurgPYCkQ"])
+            .then(function (data) {
+
+                console.log(data);
+          })
+          .catch(function (e) {
+                console.error(e.message);
+          });
+    
+}
+
+
+
+
+//Wallet
+if (process.argv[2] == "listaccounts") {
+        // node ravencoin listaccounts configfile=./data/ravencoinconfig.txt
+        rpc("listaccounts", [])
+            .then(function (data) {
+
+                console.log(data);
+          })
+          .catch(function (e) {
+                console.error(e.message);
+          });  
+}
+
+if (process.argv[2] == "getwalletinfo") {
+        // node ravencoin getwalletinfo configfile=./data/ravencoinconfig.txt
+        rpc("getwalletinfo", [])
+            .then(function (data) {
+
+                console.log(data);
+          })
+          .catch(function (e) {
+                console.error(e.message);
+          });  
+}
+
+
+
+
+
+if (process.argv[2] == "transferfromaddress") {
+    //  node ravencoin transferfromaddress configfile=./data/ravencoinconfig.txt
+        
+        rpc("transferfromaddress", ["$MEGATECH", "n4dQ28t5LyJf3pW5w6m8F9mvXaQ6ksWEwM", 20, "munskw5HqVKW5KKuALq5etxRECurgPYCkQ"])
+            .then(function (data) {
+
+                console.log(data);
+          })
+          .catch(function (e) {
+                console.error(e.message);
+          });
+    //mvzRFJRNPY2JjTi3gY46ofskBnxC6WHpyz
+}
+
+
+
+async function rpc(method, params) {
+    
+      const promise = new Promise((resolutionFunc, rejectionFunc) => {
+
+        const options = {
+              auth: {
+                    username: CONFIG.rpcUsername,
+                    password: CONFIG.rpcPassword,
+              },
+        };
+        const data = {
+              jsonrpc: "1.0",
+              id: CONFIG.asset,
+              method,
+              params,
+        };
+
+        try {
+
+                  const rpcResponse = axios.post("http://127.0.0.1:18766", data, options);
+
+                  rpcResponse.then((re) => {
+                        const result = re.data.result;
+                        resolutionFunc(result);
+                  });
+                  rpcResponse.catch((e) => {
+                        rejectionFunc(e);
+                  });
+
+        } catch (e) {
+              console.log("11111111111111111111")
+              rejectionFunc(e);
+        }
+      });
+      return promise;
+    
+}    
+
+
+
+function getConfig() {
+  let config = null;
+  for (const argument of process.argv) {
+    if (argument.startsWith("configfile=") === true) {
+      const fileName = argument.replace("configfile=", "");
+      if (fs.existsSync(fileName) === false) {
+        console.error("Could not locate file", fileName);
+        process.exit(1);
+      }
+      config = JSON.parse(fs.readFileSync(fileName, "utf8"));
+    }
+  }
+
+  if (config === null) {
+    const message = `Could not locate the config file.
+    Please start this app with "node index configfile=FILE_PATH"
+    
+    The config file should look like
+    
+    {
+      "rpcUsername": "YOUR_USERNAME",
+      "rpcPassword": "YOUR VERY SECRET PASSWORD",  
+      "asset": "YOUR COOL ASSET"
+    }
+    
+    `;
+    console.error(message);
+    process.exit(1);
+  }
+
+  return config;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+First setup Ravencoin wallet server as 
+
+server=1
+whitelist=127.0.0.1
+txindex=1
+addressindex=1
+assetindex=1
+timestampindex=1
+spentindex=1
+rpcuser=supermegasecret
+rpcpassword=supermegasecret
+
+
+then run the server with    ./raven-qt -testnet 
+
+
+*/
+
