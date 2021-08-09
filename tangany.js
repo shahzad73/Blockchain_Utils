b3210981710cf5ea175b97a1c7bfd8f6606b2485
Shahzad Aslam <sa@digishares.io>
2021-08-09 09:54:48 +0500

    change 234

--- tangany.js
+++ tangany.js
@@ -8,22 +8,20 @@ const axios = require("axios");
 
 */
 
+
+
+
 if (process.argv[2] == "test") {
 		//node tangany test
 	
-		axios.post("https://api.tangany.com/v1/wallets", {
+		axios.post("https://api.tangany.com/v1/eth/erc20/0x4dF75AEE40F7C68D6b5D75E33e011ddAbF1fFCCf/Wallet1", {
 			"headers": {
-				"Content-Type": "application/json",
 				"tangany-client-id": "464ac44d-9d4d-4146-8882-398e8f1d3281",
 				"tangany-client-secret": "UBnL.TfwXY1EN7llf3acm6dQ-0-l8-3Q55",
 				"tangany-vault-url": "https://cw-keyv-demo-xin-fin.vault.azure.net",
 				"tangany-subscription": "74350b79022c42d8b1da7f5f771091cc"
 			}, "data": {
-				"wallet": "Wallet1",
-				"useHsm": false,
-				"tags": [
-					{"hasKYC": true}
-				]
+
 			}
 		}).then(response => {
 				console.log(response.data);
